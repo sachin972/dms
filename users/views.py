6ff15dd6310af9557b88f08e20387dfaafe521ed
Sachin Tripathi <sachintripathi972000@gmail.com>
2024-02-24 14:28:36 +0530

    adding migrations and updates

--- users/views.py
+++ users/views.py
@@ -2,5 +2,12 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(req):
+def SignUP(req):
     return JsonResponse({'message': "Run successfully"})
+
+def SignIN(req, res):
+    return HttpResponse("Success")
+
+
+def UpdateRole(req, res):
+    return HttpResponse("Role Updated")
